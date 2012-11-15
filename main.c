cb98b20d1e770eddbd14538b3ebd41e031905b70
Bjørn Forsman <bjorn.forsman@prevas.no>
2012-11-15 10:05:06 +0100

    Use MJPEG instead of YUV for better quality / less bandwidth
    Try to grab MJPEG frames from device instead of YUV. If MJPEG is not
    supported, fall back to YUV.
    
    NOTE: To make a valid .jpeg structure from a webcam MPEG frame (so that
    libjpeg can decompress it), do the following.
      1. Start with (fixed) jpg header
      2. Append (fixed) huffman table
      3. Append MJPEG frame data, but skip the two byte start-of-image (SOI) marker

--- main.c
+++ main.c
@@ -111,6 +111,7 @@ static int do_fft = 0;
 static int find_barcode = 0;
 static int barcode_algorithm;
 static char* deviceName = "/dev/video0";
+static char use_mjpeg = 1; // try to use MJPEG data, if that fails fall back to YUV
 
 static void captureStop(void);
 static void deviceUninit(void);
@@ -123,6 +124,136 @@ void sighandler(int signum)
 	deviceUninit();
 	deviceClose();
 	exit(EXIT_SUCCESS);
+}
+
+unsigned char jpg_header[] = {
+	0xff, 0xd8,                   // SOI
+	0xff, 0xe0,                   // APP0
+	0x00, 0x10,                   // APP0 Hdr size
+	0x4a, 0x46, 0x49, 0x46, 0x00, // ID string: 'JFIF' + null terminator
+	0x01, 0x01,                   // Version
+	0x00,                         // Bits per type
+	0x00, 0x00,                   // X density
+	0x00, 0x00,                   // Y density
+	0x00,                         // X Thumbnail size
+	0x00                          // Y Thumbnail size
+};
+
+/* JPEG DHT Segment for YCrCb omitted from MJPEG data */
+static unsigned char jpeg_dht_seg[] = {
+	0xff, 0xc4, 0x01, 0xa2,
+
+	0x00, 0x00, 0x01, 0x05, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b,
+
+	0x01, 0x00, 0x03, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00,
+	0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b,
+
+	0x10, 0x00, 0x02, 0x01, 0x03, 0x03, 0x02, 0x04, 0x03, 0x05, 0x05, 0x04, 0x04, 0x00, 0x00, 0x01, 0x7d,
+	0x01, 0x02, 0x03, 0x00, 0x04, 0x11, 0x05, 0x12, 0x21, 0x31, 0x41, 0x06, 0x13, 0x51, 0x61, 0x07,
+	0x22, 0x71, 0x14, 0x32, 0x81, 0x91, 0xa1, 0x08, 0x23, 0x42, 0xb1, 0xc1, 0x15, 0x52, 0xd1, 0xf0,
+	0x24, 0x33, 0x62, 0x72, 0x82, 0x09, 0x0a, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x25, 0x26, 0x27, 0x28,
+	0x29, 0x2a, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3a, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49,
+	0x4a, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5a, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69,
+	0x6a, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 0x7a, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89,
+	0x8a, 0x92, 0x93, 0x94, 0x95, 0x96, 0x97, 0x98, 0x99, 0x9a, 0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7,
+	0xa8, 0xa9, 0xaa, 0xb2, 0xb3, 0xb4, 0xb5, 0xb6, 0xb7, 0xb8, 0xb9, 0xba, 0xc2, 0xc3, 0xc4, 0xc5,
+	0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xd2, 0xd3, 0xd4, 0xd5, 0xd6, 0xd7, 0xd8, 0xd9, 0xda, 0xe1, 0xe2,
+	0xe3, 0xe4, 0xe5, 0xe6, 0xe7, 0xe8, 0xe9, 0xea, 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7, 0xf8,
+	0xf9, 0xfa,
+
+	0x11, 0x00, 0x02, 0x01, 0x02, 0x04, 0x04, 0x03, 0x04, 0x07, 0x05, 0x04, 0x04, 0x00, 0x01, 0x02, 0x77,
+	0x00, 0x01, 0x02, 0x03, 0x11, 0x04, 0x05, 0x21, 0x31, 0x06, 0x12, 0x41, 0x51, 0x07, 0x61, 0x71,
+	0x13, 0x22, 0x32, 0x81, 0x08, 0x14, 0x42, 0x91, 0xa1, 0xb1, 0xc1, 0x09, 0x23, 0x33, 0x52, 0xf0,
+	0x15, 0x62, 0x72, 0xd1, 0x0a, 0x16, 0x24, 0x34, 0xe1, 0x25, 0xf1, 0x17, 0x18, 0x19, 0x1a, 0x26,
+	0x27, 0x28, 0x29, 0x2a, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3a, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48,
+	0x49, 0x4a, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5a, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68,
+	0x69, 0x6a, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 0x7a, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87,
+	0x88, 0x89, 0x8a, 0x92, 0x93, 0x94, 0x95, 0x96, 0x97, 0x98, 0x99, 0x9a, 0xa2, 0xa3, 0xa4, 0xa5,
+	0xa6, 0xa7, 0xa8, 0xa9, 0xaa, 0xb2, 0xb3, 0xb4, 0xb5, 0xb6, 0xb7, 0xb8, 0xb9, 0xba, 0xc2, 0xc3,
+	0xc4, 0xc5, 0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xd2, 0xd3, 0xd4, 0xd5, 0xd6, 0xd7, 0xd8, 0xd9, 0xda,
+	0xe2, 0xe3, 0xe4, 0xe5, 0xe6, 0xe7, 0xe8, 0xe9, 0xea, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7, 0xf8,
+	0xf9, 0xfa
+};
+
+
+/*
+ * Convert mjpeg image buffer (from webcam) to raw pixel buffer (RGB888)
+ *
+ * To make a valid .jpeg structure from a webcam MPEG frame (so that libjpeg
+ * can decompress it), do the following.
+ * 1. Start with (fixed) jpg header
+ * 2. Append (fixed) huffman table
+ * 3. Append MJPEG frame data, but skip the two byte start-of-image (SOI) marker
+ *
+ * @param out_buf points to malloc'ed data (if call succeeds), user is responsible for free()
+ * @return 0 success
+ * @return -1 error
+ */
+int MJPEGtoRGB888(unsigned char *mjpeg_frame, size_t mjpeg_frame_len, int width, int height, unsigned char **out_buf)
+{
+	int x;
+	unsigned char r, g, b;
+	struct jpeg_decompress_struct cinfo;
+	struct jpeg_error_mgr jerr;
+	JSAMPARRAY pJpegBuffer; /* Output row buffer */
+	int row_stride;         /* physical row width in output buffer */
+	unsigned char jpeg_file_buffer[1024*1024*1];
+	int offset = 0;
+
+	cinfo.err = jpeg_std_error(&jerr);
+	jpeg_create_decompress(&cinfo);
+
+	// To make a valid .jpeg structure from a webcam MPEG frame, do the following.
+
+	// 1. Start with (fixed) jpg header
+	memcpy(jpeg_file_buffer, jpg_header, sizeof jpg_header);
+
+	// 2. Append (fixed) huffman table
+	memcpy(jpeg_file_buffer + sizeof jpg_header, jpeg_dht_seg, sizeof jpeg_dht_seg);
+
+	// 3. Append MJPEG frame data, but skip the two byte start-of-image (SOI) marker
+	memcpy(jpeg_file_buffer + sizeof jpg_header + sizeof jpeg_dht_seg,
+			mjpeg_frame + 2,
+			mjpeg_frame_len - 2);
+
+	jpeg_mem_src(&cinfo, jpeg_file_buffer, (unsigned long)mjpeg_frame_len + sizeof jpg_header + sizeof jpeg_dht_seg);
+
+	if (JPEG_HEADER_OK != jpeg_read_header(&cinfo, 1)) {
+		printf("jpeg_read_header: error\n");
+		return -1;
+	}
+
+	jpeg_start_decompress(&cinfo);
+	*out_buf = malloc(sizeof (unsigned char) * width * height * 3); // 3 bytes per pixel for RGB888
+
+	if (!*out_buf){
+		printf("%s: out of memory\n", __func__);
+		return -1;
+	}
+	row_stride = width * cinfo.output_components ;
+	pJpegBuffer = (*cinfo.mem->alloc_sarray)((j_common_ptr) &cinfo, JPOOL_IMAGE, row_stride, 1);
+
+	while (cinfo.output_scanline < cinfo.output_height) {
+		jpeg_read_scanlines(&cinfo, pJpegBuffer, 1);
+		for (x = 0; x < width; x++) {
+			r = pJpegBuffer[0][cinfo.output_components * x];
+			if (cinfo.output_components > 2) {
+				g = pJpegBuffer[0][cinfo.output_components * x + 1];
+				b = pJpegBuffer[0][cinfo.output_components * x + 2];
+			} else {
+				g = r;
+				b = r;
+			}
+			*(*out_buf + offset++) = r;
+			*(*out_buf + offset++) = g;
+			*(*out_buf + offset++) = b;
+		}
+	}
+
+	jpeg_finish_decompress(&cinfo);
+	jpeg_destroy_decompress(&cinfo);
+	return 0;
 }
 
 
@@ -809,7 +940,7 @@ int do_find_barcode1(struct surface *surf)
 /**
 	process image read
 */
-static int imageProcess(const void* p)
+static int imageProcess(const void *p, size_t buflen)
 {
 	static int skip_frames = 1; // the first frame(s) is usually bad, so skip it
 	unsigned char* src = (unsigned char*)p;
@@ -817,22 +948,26 @@ static int imageProcess(const void* p)
 	unsigned int i, line, offset, offset_in_line;
 	char tmp;
 	struct surface surf;
+	int ret;
 
 	if (skip_frames-- > 0) {
 		return 0;
 	}
 
-	dst = malloc(width*height*3*sizeof(char));
-	if (dst == NULL) {
-		perror("malloc");
-		return -1;
-	}
+	if (use_mjpeg) {
+		ret = MJPEGtoRGB888(src, buflen, width, height, &dst);
+		if (ret != 0) {
+			return -1;
+		}
+	} else {
+		dst = malloc(width*height*3*sizeof(char));
+		if (dst == NULL) {
+			perror("malloc");
+			return -1;
+		}
 
-	//printf("imageProcess() start\n");
-	//fflush(NULL);
-	//printf("  convert from YUV422 to RGB888\n");
-	// convert from YUV422 to RGB888
-	YUV422toRGB888(width,height,src,dst);
+		YUV422toRGB888(width, height, src, dst);
+	}
 
 	// swap pixels
 	if (swap_pixels) {
@@ -963,7 +1098,7 @@ static int frameRead(void)
 				}
 			}
 
-			ret = imageProcess(buffers[0].start);
+			ret = imageProcess(buffers[0].start, buffers[0].length);
 			break;
 #endif
 
@@ -990,7 +1125,7 @@ static int frameRead(void)
 
 			assert(buf.index < n_buffers);
 
-			ret = imageProcess(buffers[buf.index].start);
+			ret = imageProcess(buffers[buf.index].start, buffers[buf.index].length);
 
 			if (-1 == xioctl(fd, VIDIOC_QBUF, &buf))
 				errno_exit("VIDIOC_QBUF");
@@ -1025,7 +1160,7 @@ static int frameRead(void)
 
 				assert (i < n_buffers);
 
-				ret = imageProcess((void *)buf.m.userptr);
+				ret = imageProcess((void *)buf.m.userptr, buffers[i].length);
 
 				if (-1 == xioctl(fd, VIDIOC_QBUF, &buf))
 					errno_exit("VIDIOC_QBUF");
@@ -1408,8 +1543,19 @@ static void deviceInit(void)
 	fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
 	fmt.fmt.pix.width = width; 
 	fmt.fmt.pix.height = height;
-	fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_YUYV; //V4L2_PIX_FMT_RGB555X; //V4L2_PIX_FMT_RGB24;//  V4L2_PIX_FMT_YUYV
 	fmt.fmt.pix.field = V4L2_FIELD_SEQ_TB;
+
+	if (use_mjpeg) {
+		fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_MJPEG;
+		if (-1 == xioctl(fd, VIDIOC_S_FMT, &fmt)) {
+			printf("info: MJPEG not supported, falling back to YUV\n");
+			fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_YUYV;
+			use_mjpeg = 0;
+		}
+	} else {
+		fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_YUYV;
+		use_mjpeg = 0;
+	}
 
 	if (-1 == xioctl(fd, VIDIOC_S_FMT, &fmt))
 		errno_exit("VIDIOC_S_FMT");
